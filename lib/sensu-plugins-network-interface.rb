48790a1a7ac083e0265a11045491552545f8d96f
Matteo Cerutti <matteo.cerutti@hotmail.co.uk>
2015-12-28 15:00:57 +0100

    Adding changelog, license etc.

---
+++ lib/sensu-plugins-network-interface.rb
@@ -0,0 +1,1 @@
+require 'sensu-plugins-network-interface/version'
---
+++ lib/sensu-plugins-network-interface/version.rb
@@ -0,0 +1,9 @@
+module SensuPluginsNetworkInterface
+  module Version
+    MAJOR = 0
+    MINOR = 1
+    PATCH = 1
+
+    VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
+  end
+end
